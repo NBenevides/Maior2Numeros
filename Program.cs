171e5a7a2b909db6992f268a06d005a2c8c15a35
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-26 19:49:01 -0300

    Exercicio Completo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int numero1, numero2, maiorNumero;
+
+Console.Write("Digite o primeiro numero: ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo numero: ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+if (numero1 > numero2)
+{
+    maiorNumero = numero1;
+}
+else
+{
+    maiorNumero = numero2;
+
+}
+
+Console.WriteLine($"O maior numero e o: {maiorNumero}");
